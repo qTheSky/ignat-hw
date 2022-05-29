860e2b8223b580ec88f774a5b6fffb0ffc7a19d6
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-05-29 13:46:36 +0300

    add scss, change button styles

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom';
 import {PATH} from './Pages';
+import s from './Header.module.scss'
 
 function Header() {
 		return (
 				<header>
-						<NavLink to={PATH.PRE_JUNIOR}>Pre Junior </NavLink>
-						<NavLink to={PATH.JUNIOR}>Junior </NavLink>
-						<NavLink to={PATH.JUNIOR_PLUS}>Junior Plus</NavLink>
+						<nav>
+								<ul className={s.nav}>
+										<li><NavLink to={PATH.PRE_JUNIOR}>Pre Junior</NavLink></li>
+										<li><NavLink to={PATH.JUNIOR}>Junior</NavLink></li>
+										<li><NavLink to={PATH.JUNIOR_PLUS}>Junior Plus</NavLink></li>
+								</ul>
+						</nav>
 				</header>
 		)
 }
